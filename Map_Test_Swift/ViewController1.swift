9e136fc7f5c02df77211d77346814b7e911ab270
水野 駿 <mizunoshun@fagotto.local>
2015-09-15 16:18:36 +0900

    First

---
+++ Map_Test_Swift/ViewController1.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController1.swift
+//  Map_Test_Swift
+//
+//  Created by 水野 駿 on 2015/09/15.
+//  Copyright (c) 2015年 水野 駿. All rights reserved.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
