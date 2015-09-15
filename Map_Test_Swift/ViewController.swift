cdf0e0918edb8caa9dc4209f4b8862554d5e8383
水野 駿 <mizunoshun@fagotto.local>
2015-09-15 15:59:20 +0900

    Initial Commit

---
+++ Map_Test_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map_Test_Swift
+//
+//  Created by 水野 駿 on 2015/09/15.
+//  Copyright (c) 2015年 水野 駿. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
